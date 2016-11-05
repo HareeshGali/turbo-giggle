436f4d4880aa864791dc4f3f8869e66b740a2d2c
Drew Atkinson <drewatkinson5@gmail.com>
2016-11-04 23:42:56 -0500

    add method definitions

--- turbo-giggle/turbo-giggle.py
+++ turbo-giggle/turbo-giggle.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/createSession', method='POST')
+def create_session():
+    pass
+
+
+@app.route('/form', methods=['POST', 'GET'])
+def form():
+    pass
