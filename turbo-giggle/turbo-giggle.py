67365db0daa0ee8f4721067099e4f7dcf0c54fc0
Drew Atkinson <drewatkinson5@gmail.com>
2016-11-05 00:08:51 -0500

    add database functions

--- turbo-giggle/turbo-giggle.py
+++ turbo-giggle/turbo-giggle.py
@@ -1,6 +1,20 @@
-from flask import Flask, request
-app = Flask(__name__)
+import os
+import sqlite3
+from flask import Flask, g
 
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'turbo-giggle.db'),
+    SECRET_KEY='\x99n\x82\x9bG\xa7{+\xe2,\xf4\xf7|\x96\xa0\xeaD\xa9\xd9\xdae',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+
+
+# Routes
 
 @app.route('/')
 def hello_world():
@@ -15,3 +29,43 @@ def create_session():
 @app.route('/form', methods=['POST', 'GET'])
 def form():
     pass
+
+
+# Database Functions
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+# Command line commands
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+print 'Initialized the database.'
